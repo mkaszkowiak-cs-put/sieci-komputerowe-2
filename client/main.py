30752636f6da2da9ff8a85086b75c66b15c297bd
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-01-08 19:51:37 +0100

    Make client frontend responsive and add TODOs

--- client/main.py
+++ client/main.py
@@ -13,14 +13,13 @@ def encode_response(response: http_parser.Response):
     # I don't know the appropriate way to do this with Eel,
     # so we'll just encode then decode a JSON object
     return json.dumps({
-        "code": 200, # todo
+        "code": 200, # TODO: parse response code
         "raw": response.raw,
         "headers": response.headers,
         "content_length": response.content_length,
         "valid": response.valid,
         "body": response.body,
     })
-
 
 
 @eel.expose
@@ -32,9 +31,12 @@ def get_homepage():
     print("Calling GET /")
     response = client.get_homepage()
     print(response)
-    
+
     parsed_response = http_parser.parse_response(response)
     return encode_response(parsed_response)
+
+# TODO: We need a way to send files
+# See: https://stackoverflow.com/questions/59143267/python3-js-how-do-i-handle-local-file-uploads-with-eel
 
 # Remember that functions must be exposed prior to start
 eel.init('web')
